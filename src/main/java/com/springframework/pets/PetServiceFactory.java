fc39286577cbf56e8f1c9ebed809fbd706f173ee
aray.baimolda <baimolda.2001@gmail.com>
2022-05-30 05:24:30 +0600

    lesson 76: using factory beans

---
+++ src/main/java/com/springframework/pets/PetServiceFactory.java
@@ -0,0 +1,11 @@
+package com.springframework.pets;
+
+public class PetServiceFactory {
+
+    public PetService getPetService(String petType) {
+        if ("cat".equals(petType)) {
+            return new CatPetService();
+        }
+        return new DogPetService();
+    }
+}
