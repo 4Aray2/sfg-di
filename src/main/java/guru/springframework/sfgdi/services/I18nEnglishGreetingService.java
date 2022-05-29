c1727672fa14de7f28727f9a96d729e787da30c7
aray.baimolda <baimolda.2001@gmail.com>
2022-05-30 04:46:40 +0600

    lesson 75: used Configuration for DI

--- src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18nEnglishGreetingService.java
@@ -1,8 +1,21 @@
 package guru.springframework.sfgdi.services;
 
+import guru.springframework.sfgdi.repositories.EnglishGreetingRepository;
+
 public class I18nEnglishGreetingService implements GreetingService {
+
+    private final EnglishGreetingRepository englishGreetingRepository;
+
+    public I18nEnglishGreetingService(EnglishGreetingRepository englishGreetingRepository) {
+        this.englishGreetingRepository = englishGreetingRepository;
+    }
+
     @Override
     public String sayGreeting() {
         return "Hello World - EN";
+    }
+
+    public EnglishGreetingRepository getEnglishGreetingRepository() {
+        return englishGreetingRepository;
     }
 }
