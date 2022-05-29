8607fe93fee54f51f41034d0638d867b9ed3debd
aray.baimolda <baimolda.2001@gmail.com>
2022-05-30 04:15:07 +0600

    lesson 74: used Configuration for i18nService, optimized imports

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,13 +1,37 @@
 package guru.springframework.sfgdi.config;
 
-import guru.springframework.sfgdi.services.ConstructorGreetingService;
-import guru.springframework.sfgdi.services.PropertyGreetingService;
-import guru.springframework.sfgdi.services.SetterGreetingService;
+import guru.springframework.sfgdi.services.*;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"EN", "default"})
+    @Bean("i18nService")
+    I18nEnglishGreetingService i18nEnglishGreetingService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Profile("KZ")
+    @Bean("i18nService")
+    I18nKazakhGreetingService i18nKazakhGreetingService() {
+        return new I18nKazakhGreetingService();
+    }
+
+    @Profile("ES")
+    @Bean("i18nService")
+    I18NSpanishService i18NSpanishService() {
+        return new I18NSpanishService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService() {
